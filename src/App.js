8b9131fc18273c5872975619826c94d39cae369a
Reetool16 <reetoolgupta16.com>
2023-11-24 17:53:25 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,50 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Reconsilation from './reconsilation'
+// import Clback from './clback';
+// import FirstContxt from './firstcontxt';
+// import Memo2 from './Memo2';
+// import Main from './Main.js';
+// import Main2 from './Main2.js';
+// import Main3 from './Main3';
+// import Effect from './Effect';
+// import Effect2 from './Effect2';
+// import App1 from './context/App1';
+// import Memo from './Memo';
+// import Reference from './refer';
+// import Fetch from './fetch';
+// import Axios from './axios';
+import Post from './post';
+// import Axiospost from './axiospost';
+// import Axiospost2 from './axiospost2';
+// import Delete from './delete';
+// import Axiosdlt from './axiosdlt';
+import Put from './put';
+function App() {
+  return (
+    <>
+      {/* <Main /> */}
+      {/* <Main2 /> */}
+      {/* <Main3 /> */}
+      {/* <Effect /> */}
+      {/* <Effect2 /> */}
+      {/* <App1 /> */}
+      {/* {<FirstContxt />} */}
+      {/* <Memo /> */}
+      {/* <Memo2 /> */}
+      {/* <Clback /> */}
+      {/* <Reference /> */}
+      {/* <Reconsilation /> */}
+      {/* <Fetch /> */}
+      {/* <Axios /> */}
+      <Post />
+      {/* <Axiospost /> */}
+      {/* <Axiospost2 /> */}
+      {/* <Delete /> */}
+      {/* <Axiosdlt /> */}
+      <Put />
+    </>
+  );
+}
+
+export default App;
